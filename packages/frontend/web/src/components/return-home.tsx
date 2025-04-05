5bc491f62c78ef1936d29d18cc36bb71e0a6edb2
TheoSkymel <erigoni.t@gmail.com>
2025-04-05 10:44:28 +0200

    return home, correction, mobile a verifier

--- packages/frontend/web/src/components/return-home.tsx
+++ packages/frontend/web/src/components/return-home.tsx
@@ -14,9 +14,9 @@ export default function ReturnHome() {
       <button
         type='button'
         onClick={handleClick}
-        className='bg-tertiary-gray w-14 rounded-2xl p-1 shadow-[0px_4px_4px_rgba(0,0,0,0.25)]'
+        className='bg-tertiary-gray xl:bg-tertiary-gray rounded-2xl p-1 shadow-[0px_4px_4px_rgba(0,0,0,0.25)]'
       >
-        <img src={backImg} alt='' />
+        <img src={backImg} alt='' className='lg:w- w-9 sm:w-9 xl:w-13' />
       </button>
     </div>
   );
