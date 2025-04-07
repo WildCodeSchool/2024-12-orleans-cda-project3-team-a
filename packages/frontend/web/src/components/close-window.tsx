330e905ff7b915b92c50fa564824cc76507516f6
YahyaBlm <yahya.belmou@gmail.com>
2025-04-07 16:30:55 +0200

    button close-window + global.css fonts

--- packages/frontend/web/src/components/close-window.tsx
+++ packages/frontend/web/src/components/close-window.tsx
@@ -14,10 +14,10 @@ export default function Close() {
   return (
     <button
       type='button'
-      className='bg-tertiary-gray flex h-10 w-10 cursor-pointer items-center justify-center rounded-lg shadow-[0px_4px_4px_rgba(0,0,0,0.25)] active:shadow-none md:h-14 md:w-14 md:rounded-2xl'
+      className='bg-tertiary-gray flex h-10 w-10 cursor-pointer items-center justify-center rounded-lg shadow-[0px_4px_4px_rgba(0,0,0,0.25)] active:shadow-none md:h-12 md:w-12 md:rounded-2xl'
       onClick={closeWindow}
     >
-      <img src={close} alt='close' className='w-6 md:w-9' />
+      <img src={close} alt='close' className='w-5 md:w-7' />
     </button>
   );
 }
