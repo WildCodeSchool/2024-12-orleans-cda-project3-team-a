08b3a576273d041c93f316e223d043fed42ca545
YahyaBlm <yahya.belmou@gmail.com>
2025-04-07 16:49:57 +0200

    button-buy

---
+++ packages/frontend/web/src/components/button-buy.tsx
@@ -0,0 +1,15 @@
+import { PropsWithChildren } from "react";
+
+type ButtonBuyProps = PropsWithChildren;
+
+export default function ButtonBuy({ children }: ButtonBuyProps) {
+  return (
+    <button
+      type='button'
+      className='cursor-pointer items-center justify-center rounded-md border border-black bg-[linear-gradient(to_bottom,_#FFFFFF,_#D8D8D8)] px-2 shadow-[0px_4px_4px_rgba(0,0,0,0.25)] active:shadow-none'
+      //   onClick={''}
+    >
+      <div className='flex items-center justify-center'>{children}</div>
+    </button>
+  );
+}
