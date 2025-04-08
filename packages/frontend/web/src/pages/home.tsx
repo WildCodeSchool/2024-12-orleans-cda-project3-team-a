ac3bfdb6c0b6da926efdb2207f68547545f6b93d
YahyaBlm <yahya.belmou@gmail.com>
2025-04-08 11:48:34 +0200

    last fix

--- packages/frontend/web/src/pages/home.tsx
+++ packages/frontend/web/src/pages/home.tsx
@@ -1,37 +1,9 @@
-import { useEffect, useState } from 'react';
-
-import type { SomeInterface } from '@app/shared';
-
-const API_URL = import.meta.env.VITE_API_URL;
-
 export default function Home() {
-  const [someData, setSomeData] = useState<SomeInterface>({
-    someProperty: 'someValue',
-  });
-
-  useEffect(() => {
-    const abortController = new AbortController();
-
-    (async () => {
-      const response = await fetch(`${API_URL}/api/demo`, {
-        signal: abortController.signal,
-      });
-      const data = await response.json();
-      setSomeData(data);
-    })();
-
-    return () => {
-      abortController.abort();
-    };
-  }, []);
-
   return (
-    <div className='font-display flex h-screen w-screen flex-col items-center justify-center gap-4'>
-      <div className='rounded-2xl bg-red-500 p-1 px-2 text-white transition-all hover:bg-green-400 active:bg-amber-500'>
-        {'Edit pages/home.tsx to edit this screen'}
-      </div>
-
-      <div>{someData.someProperty}</div>
+    <div className='flex items-center justify-center p-4'>
+      <p className='font-aerokids bg-[linear-gradient(to_right,_#f75a61,_#68c9f4,_#92d635,_#e8bd41,_#b19ebb)] bg-clip-text p-7 text-9xl text-transparent'>
+        Fantasy Park !
+      </p>
     </div>
   );
 }
