e0d2acb18043a14e00bac01f5f4a68667d5a08e8
Chloe Seclier <seclier.chloe@gmail.com>
2025-04-08 22:13:19 +0200

    feature/nb-visitors-moons

--- packages/frontend/web/src/pages/home.tsx
+++ packages/frontend/web/src/pages/home.tsx
@@ -1,9 +1,17 @@
+import InfoNbVisitorsMoons from '@/components/info-nb-visitors-moons';
+
 export default function Home() {
   return (
-    <div className='flex items-center justify-center p-4'>
-      <p className='font-aerokids bg-[linear-gradient(to_right,var(--color-winged-red),var(--color-fairy-blue),var(--color-fairy-green),var(--color-title-orange),var(--color-title-purple))] bg-clip-text p-7 text-9xl text-transparent'>
-        {'Fantasy Park !'}
-      </p>
-    </div>
+    <>
+      <div className='flex items-center justify-center p-4'>
+        <p className='font-aerokids bg-[linear-gradient(to_right,var(--color-winged-red),var(--color-fairy-blue),var(--color-fairy-green),var(--color-title-orange),var(--color-title-purple))] bg-clip-text p-7 text-9xl text-transparent'>
+          {'Fantasy Park !'}
+        </p>
+      </div>
+      <div>
+        {' '}
+        <InfoNbVisitorsMoons />
+      </div>
+    </>
   );
 }
