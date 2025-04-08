01b6ae52a75883b806b84f4fd0dfc39bc1bc5e0d
YahyaBlm <yahya.belmou@gmail.com>
2025-04-08 14:38:43 +0200

    gradient with vars + font link rectified

--- packages/frontend/web/src/pages/home.tsx
+++ packages/frontend/web/src/pages/home.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className='flex items-center justify-center p-4'>
-      <p className='font-aerokids bg-[linear-gradient(to_right,_#f75a61,_#68c9f4,_#92d635,_#e8bd41,_#b19ebb)] bg-clip-text p-7 text-9xl text-transparent'>
+      <p className='font-aerokids bg-[linear-gradient(to_right,var(--color-winged-red),var(--color-fairy-blue),var(--color-fairy-green),var(--color-title-orange),var(--color-title-purple))] bg-clip-text p-7 text-9xl text-transparent'>
         {'Fantasy Park !'}
       </p>
     </div>
