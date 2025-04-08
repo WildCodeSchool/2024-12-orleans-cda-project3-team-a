f2b55e0a9a2a90314aaef8ff4e104a3cf9f93c21
YahyaBlm <yahya.belmou@gmail.com>
2025-04-08 12:23:19 +0200

    last fix

--- packages/frontend/web/src/pages/home.tsx
+++ packages/frontend/web/src/pages/home.tsx
@@ -2,7 +2,7 @@ export default function Home() {
   return (
     <div className='flex items-center justify-center p-4'>
       <p className='font-aerokids bg-[linear-gradient(to_right,_#f75a61,_#68c9f4,_#92d635,_#e8bd41,_#b19ebb)] bg-clip-text p-7 text-9xl text-transparent'>
-        Fantasy Park !
+        {"Fantasy Park !"}
       </p>
     </div>
   );
