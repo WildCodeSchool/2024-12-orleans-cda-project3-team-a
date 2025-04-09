a133c080569efb551951b8940cf476e1205460ec
SkymelTheo <erigoni.t@gmail.com>
2025-04-09 11:26:25 +0200

    input correction

--- packages/frontend/web/src/components/input-blue.tsx
+++ packages/frontend/web/src/components/input-blue.tsx
@@ -1,15 +1,20 @@
-import { useState } from 'react';
+import type { ChangeEvent } from 'react';
 
 type InputProps = {
   readonly type: 'email' | 'password' | 'text';
   readonly placeholder: string;
+  readonly onChange: (value: string) => void;
+  readonly value: string;
 };
 
-export default function Input({ type, placeholder }: InputProps) {
-  const [inputValue, setInputValue] = useState('');
-
-  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setInputValue(event.target.value);
+export default function Input({
+  type,
+  placeholder,
+  onChange,
+  value,
+}: InputProps) {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    onChange(event.target.value);
   };
 
   return (
@@ -17,7 +22,7 @@ export default function Input({ type, placeholder }: InputProps) {
       className='border-secondary-blue bg-primary-blue focus:border-secondary-blue h-7 w-51 rounded border px-2 focus:border-2 focus:outline-none md:w-74 md:rounded-md'
       type={type}
       placeholder={placeholder}
-      value={inputValue}
+      value={value}
       onChange={handleChange}
     />
   );
--- packages/frontend/web/src/pages/test.tsx
+++ packages/frontend/web/src/pages/test.tsx
@@ -1,4 +1,4 @@
-import Input from '@/components/input';
+import Input from '@/components/input-blue';
 import ReturnHome from '@/components/return-home';
 
 export default function Test() {
