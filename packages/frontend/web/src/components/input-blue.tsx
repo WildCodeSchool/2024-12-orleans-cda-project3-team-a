95a1d87271c3358369dffb6665d6ce64cbcfa294
SkymelTheo <erigoni.t@gmail.com>
2025-04-09 11:46:42 +0200

    correction ok, nom du fichier changé

--- packages/frontend/web/src/components/input-blue.tsx
+++ packages/frontend/web/src/components/input-blue.tsx
@@ -3,27 +3,25 @@ import type { ChangeEvent } from 'react';
 type InputProps = {
   readonly type: 'email' | 'password' | 'text';
   readonly placeholder: string;
-  readonly onChange: (value: string) => void;
+  readonly onChangeInput: (value: string) => void;
   readonly value: string;
 };
 
-export default function Input({
+export default function InputBlue({
   type,
   placeholder,
-  onChange,
+  onChangeInput,
   value,
 }: InputProps) {
-  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
-    onChange(event.target.value);
-  };
-
   return (
     <input
       className='border-secondary-blue bg-primary-blue focus:border-secondary-blue h-7 w-51 rounded border px-2 focus:border-2 focus:outline-none md:w-74 md:rounded-md'
       type={type}
       placeholder={placeholder}
       value={value}
-      onChange={handleChange}
+      onChange={(event) => {
+        onChangeInput(event.target.value);
+      }}
     />
   );
 }
