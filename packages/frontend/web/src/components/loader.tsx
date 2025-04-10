2cf563c670311e999ed2be43037eab3e802cc3c2
YahyaBlm <yahya.belmou@gmail.com>
2025-04-10 14:06:56 +0200

    loader +image

--- packages/frontend/web/src/components/loader.tsx
+++ packages/frontend/web/src/components/loader.tsx
@@ -1,7 +1,11 @@
+import loader from '../assets/images/icons-buttons/loader.png';
+
 export default function Loader() {
     return (
-      <div className="w-25 h-25 text-center rounded-[50px] blur-[1px] shadow-[0px -5px 20px 0px rgb(186, 66, 255), 0px 5px 20px 0px rgb(0, 225, 255)] bg-[linear-gradient(rgb(186, 66, 255) 35%, rgb(0, 225, 255))] animation: var(--animate-spin)">
-        <div className="w-25 h-25 rounded-[50px] blur-[1px] bg-[rgb(36, 36, 36)]"></div>
+      <div className="flex items-center justify-center w-14 h-14 text-center rounded-[50px] blur-[1px] bg-[linear-gradient(to_bottom,_#BA42FF,_#00E1FF)] shadow-[0px_-5px_20px_0px_rgb(186,_66,_255),_0px_5px_20px_0px_rgb(0,_225,_255)] animate-spin">
+        <div className="w-12 h-12 rounded-[50px] blur-[1px] bg-secondary-gray"></div>
+        <img src={loader} alt="loader" className=''/>
       </div>
     );
   }
+
