79769568939ec0a3021f7b4a66ed15ff66793d93
YahyaBlm <yahya.belmou@gmail.com>
2025-04-10 15:05:15 +0200

    loader ok

--- packages/frontend/web/src/components/loader.tsx
+++ packages/frontend/web/src/components/loader.tsx
@@ -1,11 +1,10 @@
 import loader from '../assets/images/icons-buttons/loader.png';
 
 export default function Loader() {
-    return (
-      <div className="flex items-center justify-center w-14 h-14 text-center rounded-[50px] blur-[1px] bg-[linear-gradient(to_bottom,_#BA42FF,_#00E1FF)] shadow-[0px_-5px_20px_0px_rgb(186,_66,_255),_0px_5px_20px_0px_rgb(0,_225,_255)] animate-spin">
-        <div className="w-12 h-12 rounded-[50px] blur-[1px] bg-secondary-gray"></div>
-        <img src={loader} alt="loader" className=''/>
-      </div>
-    );
-  }
-
+  return (
+    <div className='flex h-12 w-12 animate-spin items-center justify-center rounded-[50px] bg-[linear-gradient(to_bottom,_#BA42FF,_#00E1FF)] text-center shadow-[0px_-5px_20px_0px_rgb(186,_66,_255),_0px_5px_20px_0px_rgb(0,_225,_255)]'>
+      <div className='bg-primary-gray h-10 w-10 rounded-[50px]'></div>
+      <img src={loader} alt='loader' className='absolute' />
+    </div>
+  );
+}
--- packages/frontend/web/src/pages/test.tsx
+++ packages/frontend/web/src/pages/test.tsx
@@ -3,11 +3,11 @@ import ReturnHome from '@/components/return-home';
 
 export default function Test() {
   return (
-    <div>
+    <div className='flex flex-col items-center justify-center gap-5'>
       <ReturnHome />
       <p>{'PAGE TEST A SUPPRIMER PLUS TARD'}</p>
       <p className='text-2xl'>{' Laisse courrir ton imagination 🦅'}</p>
-      <Loader/>
+      <Loader />
     </div>
   );
 }
