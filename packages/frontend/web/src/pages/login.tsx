aec09518debc8262683a50ad96a283971cc7c654
SkymelTheo <erigoni.t@gmail.com>
2025-04-10 09:31:12 +0200

    accueil

--- packages/frontend/web/src/pages/login.tsx
+++ packages/frontend/web/src/pages/login.tsx
@@ -4,21 +4,37 @@ import Logo from '../assets/images/logo/fp-logo-arc.png';
 export default function Login() {
   return (
     <div
-      className='h-screen bg-cover bg-center'
+      className='flex h-screen justify-center bg-cover bg-center'
       style={{ backgroundImage: `url(${BgHome})` }}
     >
       <img
         src={Logo}
         alt='Fantasy Park'
-        className='absolute h-80 min-w-266 pl-55'
+        className='absolute top-30 z-2 min-h-23 min-w-80 md:h-80 md:min-w-266'
       />
-      <div className='flex items-center justify-center pt-45'>
-        <div className='h-135 w-156 bg-white'>
-          <form>
-            <p>{'coucou'}</p>
-          </form>
+      <div className='flex items-center justify-center pt-45 md:pt-45'>
+        <div className='h-83 w-74 rounded-lg bg-white opacity-90 shadow-[0px_8px_4px_rgba(0,0,0,0.25)] md:h-135 md:w-156'>
+          <form>{'coucou'}</form>
         </div>
       </div>
     </div>
   );
 }
+
+//faire la carré blanc aussi
+
+//  w-80 ,w-266 h23
+
+// h-80, h-23
+
+// <img
+// src={Logo}
+// alt='Fantasy Park'
+// className='z-2 min-h-23 min-w-80 pt-15 md:absolute md:h-80 md:min-w-266 md:pl-55'
+// />
+// <div className='flex items-center justify-center pt-45'>
+// <div className='h-83 w-73 bg-white opacity-90 md:h-135 md:w-156'>
+//   <form>{'coucou'}</form>
+// </div>
+// </div>
+// </div>
--- packages/frontend/web/src/pages/test.tsx
+++ packages/frontend/web/src/pages/test.tsx
@@ -1,8 +1,7 @@
 import { useState } from 'react';
 
-import Input from '@/components/input-blue';
+import InputBlue from '@/components/input-blue';
 import ReturnHome from '@/components/return-home';
-import Login from '@/pages/login';
 
 export default function Test() {
   const [email, setEmail] = useState('test');
@@ -10,7 +9,7 @@ export default function Test() {
   return (
     <div>
       <ReturnHome />
-      <Input
+      <InputBlue
         type='email'
         placeholder='Votre email'
         value={email}
@@ -19,7 +18,6 @@ export default function Test() {
       {email}
       <p>{'PAGE TEST A SUPPRIMER PLUS TARD'}</p>
       <p className='text-2xl'>{' Laisse courrir ton imagination 🦅'}</p>
-      <Login />
     </div>
   );
 }
