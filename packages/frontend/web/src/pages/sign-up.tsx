a4f9801bfc010e50f5ee396cd671d5d168f28377
Chloe Seclier <seclier.chloe@gmail.com>
2025-04-10 17:52:23 +0200

    register in front ok

--- packages/frontend/web/src/pages/sign-up.tsx
+++ packages/frontend/web/src/pages/sign-up.tsx
@@ -1,9 +1,99 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import ButtonBlue from '@/components/button-blue';
+import InputBlue from '@/components/input-blue';
+
+const API_URL = import.meta.env.VITE_API_URL;
+
 export default function SignUp() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [username, setUsername] = useState('');
+
+  const navigate = useNavigate();
+
+  const signUp = async () => {
+    console.log(email, username, password, confirmPassword);
+
+    const res = await fetch(`${API_URL}/auth/register`, {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        username,
+        password,
+        confirmPassword,
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
+
+    const data = await res.json();
+
+    if (data.message === 'User registered') {
+      console.log('Redirection vers la page créer mon park');
+      await navigate('/home');
+    }
+
+    console.log(data);
+  };
+
   return (
-    <div className='flex items-center justify-center p-4'>
-      <p className='font-aerokids bg-[linear-gradient(to_right,var(--color-winged-red),var(--color-fairy-blue),var(--color-fairy-green),var(--color-title-orange),var(--color-title-purple))] bg-clip-text p-7 text-9xl text-transparent'>
-        {'Fantasy Park !'}
+    <form
+      onSubmit={async (event) => {
+        event.preventDefault();
+        await signUp();
+      }}
+      className='flex flex-col items-center justify-center gap-3 p-4'
+    >
+      <InputBlue
+        type='email'
+        placeholder='Email'
+        value={email}
+        onChangeInput={(value) => {
+          setEmail(value);
+        }}
+      />
+
+      <InputBlue
+        type='text'
+        placeholder='Pseudo'
+        value={username}
+        onChangeInput={(value) => {
+          setUsername(value);
+        }}
+      />
+
+      <InputBlue
+        type='password'
+        placeholder='Password'
+        value={password}
+        onChangeInput={(value) => {
+          setPassword(value);
+        }}
+      />
+
+      <InputBlue
+        type='password'
+        placeholder='Confirm password'
+        value={confirmPassword}
+        onChangeInput={(value) => {
+          setConfirmPassword(value);
+        }}
+      />
+
+      <ButtonBlue bg='bg-primary-blue' type='submit'>
+        {'SIGN UP'}
+      </ButtonBlue>
+
+      <p>
+        {'Already have an account ? '}
+        <a href='/' className='text-secondary-blue underline'>
+          {'Log in here.'}
+        </a>{' '}
       </p>
-    </div>
+    </form>
   );
 }
