0103f10a32d4e48f4a44012fa503772c4cdba737
YahyaBlm <yahya.belmou@gmail.com>
2025-04-10 15:25:46 +0200

    resolving conflict

--- packages/frontend/web/src/pages/test.tsx
+++ packages/frontend/web/src/pages/test.tsx
@@ -1,22 +1,10 @@
-import { useState } from 'react';
-
-import Input from '@/components/input-blue';
 import Loader from '@/components/loader';
 import ReturnHome from '@/components/return-home';
 
 export default function Test() {
-  const [email, setEmail] = useState('test');
-
   return (
     <div className='flex flex-col items-center justify-center gap-5'>
       <ReturnHome />
-      <Input
-        type='email'
-        placeholder='Votre email'
-        value={email}
-        onChangeInput={setEmail}
-      />
-      {email}
       <p>{'PAGE TEST A SUPPRIMER PLUS TARD'}</p>
       <p className='text-2xl'>{' Laisse courrir ton imagination 🦅'}</p>
       <Loader />
