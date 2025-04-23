d45163f3b2d9f05d660bb470559ca935958e8b49
SkymelTheo <erigoni.t@gmail.com>
2025-04-23 16:33:08 +0200

    fin authentification

--- packages/frontend/web/src/contexts/auth-context.tsx
+++ packages/frontend/web/src/contexts/auth-context.tsx
@@ -23,7 +23,6 @@ const API_URL = import.meta.env.VITE_API_URL;
 export default function AuthContext({ children, ...props }: AuthProviderProps) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
-  const [userConnected, setUserConnected] = useState('');
 
   useEffect(() => {
     const fetchAuth = async () => {
@@ -32,21 +31,17 @@ export default function AuthContext({ children, ...props }: AuthProviderProps) {
       });
       const data = (await res.json()) as {
         ok: boolean;
-        user?: {
-          id: string;
-          email: string;
-        };
       };
 
-      if (data.user) {
+      if (data.ok) {
         setIsLoggedIn(true);
-        setUserConnected(data.user.email);
       }
 
       setIsLoading(false);
     };
     void fetchAuth();
   }, []);
+
   const value = useMemo(
     () => ({
       isLoggedIn,
