4ce7188e3fc59036f3037db00481e46e63e68b0a
SkymelTheo <erigoni.t@gmail.com>
2025-04-28 16:39:20 +0200

    migration

---
+++ packages/backend/migrations/src/migrations/20250425135115-table-deco.ts
@@ -0,0 +1,85 @@
+import { type Kysely, sql } from 'kysely';
+
+import type { DB } from '@app/shared';
+
+export async function up(db: Kysely<DB>): Promise<void> {
+  // Migration code that update the database to the desired state.
+  await db.transaction().execute(async (trx) => {
+    await sql`
+ALTER TABLE creatures 
+ADD COLUMN background VARCHAR(50) NOT NULL;
+`.execute(trx);
+
+    await sql`
+ALTER TABLE decorations
+RENAME TO barriers;
+`.execute(trx);
+
+    await sql`
+ALTER TABLE barriers
+MODIFY price BIGINT NOT NULL ;
+`.execute(trx);
+
+    await sql`
+ALTER TABLE park_decorations 
+RENAME TO park_barriers;
+`.execute(trx);
+
+    await sql` 
+ALTER TABLE park_barriers
+RENAME COLUMN deco_id TO barrieres_id;
+`.execute(trx);
+
+    //rename park deco en park barriere
+
+    await sql` 
+CREATE TABLE decorations(
+id INT AUTO_INCREMENT PRIMARY KEY,
+name VARCHAR(150) NOT NULL,
+src_image VARCHAR(250) NOT NULL,
+creature_id INT NOT NULL,
+position VARCHAR(150) NOT NULL,
+CONSTRAINT fk_decorations_creature_id FOREIGN KEY (creature_id) REFERENCES creatures(id)
+);
+`.execute(trx);
+  });
+}
+
+export async function down(db: Kysely<DB>): Promise<void> {
+  // Migration code that reverts the database to the previous state.
+  await db.transaction().execute(async (trx) => {
+    await sql` 
+    DROP TABLE decorations
+    `.execute(trx);
+
+    await sql`
+    ALTER TABLE park_decoration
+    RENAME COLUMN barrieres_id TO deco_id
+    `.execute(trx);
+
+    await sql`
+    ALTER TABLE park_barriers
+    RENAME TO park_decorations
+    `.execute(trx);
+
+    await sql`
+      ALTER TABLE barriers
+      MODIFY price INT NOT NULL
+      `.execute(trx);
+
+    await sql`
+    ALTER TABLE park_decoration 
+    DROP COLUMN decoration_id
+    `.execute(trx);
+
+    await sql`
+    ALTER TABLE barriere
+    RENAME TO decorations
+    `.execute(trx);
+
+    await sql`
+    ALTER TABLE creatures
+    DROP COLUMN background
+    `.execute(trx);
+  });
+}
