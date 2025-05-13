1890358454b4f90dbe87fd1aa22f2c44182f0dab
Chloe Seclier <seclier.chloe@gmail.com>
2025-05-13 14:32:37 +0200

    bg for desktop and mobile

---
+++ packages/frontend/web/src/pages/page-404.tsx
@@ -0,0 +1,7 @@
+export default function Page404() {
+  return (
+    <div className='h-screen bg-[url(/images/background/404-mobile.webp)] bg-cover bg-center p-3 md:bg-[url(/images/background/404-desktop.webp)]'>
+      <p>{'coucou'}</p>
+    </div>
+  );
+}
