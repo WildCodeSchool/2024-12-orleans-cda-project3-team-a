b07b6249007e3891468f252b35eb06f527033038
SkymelTheo <erigoni.t@gmail.com>
2025-05-14 16:19:28 +0200

    pour switch

--- packages/frontend/web/src/components/buy-creature.tsx
+++ packages/frontend/web/src/components/buy-creature.tsx
@@ -1,13 +1,14 @@
 import { useState } from 'react';
 
 import Moon from '../assets/images/icons-buttons/moon.png';
+import Centaur from '../assets/images/mythologic-zone/centaur.png';
 import InputBlue from './input-blue';
 
 export default function BuyCreature() {
   const [name, setName] = useState('');
 
   return (
-    <div className='border-1'>
+    <div className='rounded-lg border-1'>
       <h1 className='p-2'>{'Buy a new Centaure'}</h1>
       <div className='flex gap-10 p-6'>
         <InputBlue
@@ -18,9 +19,11 @@ export default function BuyCreature() {
             setName(value);
           }}
         />
-        <img className='w-7' src={Moon} alt='' />
-        <button type='button'>
-          <img src='' alt='' />
+        <img className='h-7 w-7' src={Moon} alt='' />
+        <button type='button' className='w-20 rounded-lg border-2'>
+          {' '}
+          {'+'}
+          <img className='w-10' src={Centaur} alt='' />
         </button>
       </div>
     </div>
