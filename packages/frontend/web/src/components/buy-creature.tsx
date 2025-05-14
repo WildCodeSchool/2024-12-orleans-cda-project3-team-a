987ce74654e9bffb107ed5bc92ea16ea063e7c84
SkymelTheo <erigoni.t@gmail.com>
2025-05-14 17:42:08 +0200

    composant ok

--- packages/frontend/web/src/components/buy-creature.tsx
+++ packages/frontend/web/src/components/buy-creature.tsx
@@ -2,30 +2,31 @@ import { useState } from 'react';
 
 import Moon from '../assets/images/icons-buttons/moon.png';
 import Centaur from '../assets/images/mythologic-zone/centaur.png';
+import Background from './bg-menu';
+import ButtonBuy from './button-buy';
 import InputBlue from './input-blue';
 
 export default function BuyCreature() {
   const [name, setName] = useState('');
 
   return (
-    <div className='rounded-lg border-1'>
-      <h1 className='p-2'>{'Buy a new Centaure'}</h1>
-      <div className='flex gap-10 p-6'>
-        <InputBlue
-          type='text'
-          placeholder='Name'
-          value={name}
-          onChangeInput={(value) => {
-            setName(value);
-          }}
-        />
-        <img className='h-7 w-7' src={Moon} alt='' />
-        <button type='button' className='w-20 rounded-lg border-2'>
-          {' '}
-          {'+'}
+    <Background>
+      <div className='rounded-lg border-1'>
+        <h1 className='p-2'>{'Buy a new Centaure'}</h1>
+        <div className='flex items-center justify-center gap-10 p-2'>
+          <InputBlue
+            type='text'
+            placeholder='Name'
+            value={name}
+            onChangeInput={(value) => {
+              setName(value);
+            }}
+          />
+          <img className='h-7 w-7' src={Moon} alt='' />
+          <ButtonBuy /> {'+'}
           <img className='w-10' src={Centaur} alt='' />
-        </button>
+        </div>
       </div>
-    </div>
+    </Background>
   );
 }
