02653218968cb322dffa553d7b936365ed7470af
TheoSkymel <skymel81@gmail.com>
2025-05-30 10:25:38 +0200

    file getleaderboard

--- packages/backend/api/src/types.ts
+++ packages/backend/api/src/types.ts
@@ -3,4 +3,4 @@ export type * from './game/barrier/get.barriers-route';
 export type * from './game/get.enclosures';
 export type * from './game/get.decorations';
 export type * from './game/creature/get.creatures';
-export type * from './game/get.rank';
+export type * from './game/get.leaderboard';
