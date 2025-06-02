f1f42ce9e4c286d825830686ea4a8a1229a0d610
SkymelTheo <erigoni.t@gmail.com>
2025-06-02 15:00:52 +0200

    mingush plus grand, text bleu

--- packages/frontend/web/src/components/welcome-guide.tsx
+++ packages/frontend/web/src/components/welcome-guide.tsx
@@ -13,7 +13,7 @@ export default function WelcomeGuide() {
   }
   return (
     <div className='absolute top-[65%] left-[50%] w-9/10 -translate-x-1/2 -translate-y-1/2 transform rounded-md bg-white/90 p-5 text-xl md:text-2xl'>
-      <div className='flex flex-col gap-6'>
+      <div className='text-secondary-blue flex flex-col gap-3'>
         <p>{`Now that you've bestowed a name upon your magical park, it's time to bring it to life with your very first creature!`}</p>
         <p>{`For now, the Fairy realm is open to you.`}</p>
         <p>{`To unlock the next realm, the "Winged" world, you'll need to buy at least one creature of each species into your park.`}</p>
@@ -22,7 +22,7 @@ export default function WelcomeGuide() {
       <div className='relative mt-4'>
         <div className='absolute -right-4 -bottom-4'>
           <img
-            className='w-15'
+            className='w-12 md:w-20'
             src='../public/images/minguch.png'
             alt='mingush'
           />
