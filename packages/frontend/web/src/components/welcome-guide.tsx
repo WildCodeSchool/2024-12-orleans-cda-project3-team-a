e752d4d0224f7c2ef6b50afed6b818e8ac5f22da
SkymelTheo <erigoni.t@gmail.com>
2025-06-03 14:20:25 +0200

    correction animation

--- packages/frontend/web/src/components/welcome-guide.tsx
+++ packages/frontend/web/src/components/welcome-guide.tsx
@@ -14,10 +14,10 @@ export default function WelcomeGuide() {
   return (
     <div className='absolute top-[65%] left-[50%] w-3/4 -translate-x-1/2 -translate-y-1/2 transform rounded-md bg-white/90 p-5 text-xs font-bold md:text-base'>
       <div className='text-secondary-blue flex flex-col gap-3'>
-        <p className='typewriter typing1'>{`Now that you've bestowed a name upon your magical park, it's time to bring it to life with your very first creature!`}</p>
-        <p className='typewriter typing2'>{`For now, the Fairy realm is open to you.`}</p>
-        <p className='typewriter typing3'>{`To unlock the next realm, the "Winged" world, you'll need to buy at least one creature of each species into your park.`}</p>
-        <p className='typewriter typing4'>{`Let the adventure begin! ✨🐉`}</p>
+        <p className='whitespace-wrap md:animate-typing1 md:flex md:w-0 md:flex-wrap md:overflow-hidden md:whitespace-nowrap'>{`Now that you've bestowed a name upon your magical park, it's time to bring it to life with your very first creature!`}</p>
+        <p className='whitespace-wrap md:animate-typing2 md:flex md:w-0 md:flex-wrap md:overflow-hidden md:whitespace-nowrap'>{`For now, the Fairy realm is open to you.`}</p>
+        <p className='whitespace-wrap md:animate-typing3 md:flex md:w-0 md:flex-wrap md:overflow-hidden md:whitespace-nowrap'>{`To unlock the next realm, the "Winged" world, you'll need to buy at least one creature of each species into your park.`}</p>
+        <p className='whitespace-wrap md:animate-typing4 md:flex md:w-0 md:flex-wrap md:overflow-hidden md:whitespace-nowrap'>{`Let the adventure begin! ✨🐉`}</p>
       </div>
       <div className='relative mt-4'>
         <div className='absolute -right-1 -bottom-3'>
