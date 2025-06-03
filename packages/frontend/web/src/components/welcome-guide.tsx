50c308be5fbe829ce329ec21f47cef83278736df
SkymelTheo <erigoni.t@gmail.com>
2025-06-03 10:47:40 +0200

    mingush animation responsive ok

--- packages/frontend/web/src/components/welcome-guide.tsx
+++ packages/frontend/web/src/components/welcome-guide.tsx
@@ -14,13 +14,13 @@ export default function WelcomeGuide() {
   return (
     <div className='absolute top-[65%] left-[50%] w-9/10 -translate-x-1/2 -translate-y-1/2 transform rounded-md bg-white/90 p-5 text-xl md:text-2xl'>
       <div className='text-secondary-blue flex flex-col gap-3'>
-        <p className='typewriter typing1'>{`Now that you've bestowed a name upon your magical park, it's time to bring it to life with your very first creature!`}</p>
-        <p className='typewriter typing2'>{`For now, the Fairy realm is open to you.`}</p>
-        <p className='typewriter typing3'>{`To unlock the next realm, the "Winged" world, you'll need to buy at least one creature of each species into your park.`}</p>
-        <p className='typewriter typing4'>{`Let the adventure begin! ✨🐉`}</p>
+        <p className='coucou typewriter typing1'>{`Now that you've bestowed a name upon your magical park, it's time to bring it to life with your very first creature!`}</p>
+        <p className='coucou typewriter typing2'>{`For now, the Fairy realm is open to you.`}</p>
+        <p className='coucou typewriter typing3'>{`To unlock the next realm, the "Winged" world, you'll need to buy at least one creature of each species into your park.`}</p>
+        <p className='coucou typewriter typing4'>{`Let the adventure begin! ✨🐉`}</p>
       </div>
       <div className='relative mt-4'>
-        <div className='absolute -right-4 -bottom-4'>
+        <div className='absolute -right-1 -bottom-3'>
           <img
             className='w-12 md:w-20'
             src='/images/minguch.png'
