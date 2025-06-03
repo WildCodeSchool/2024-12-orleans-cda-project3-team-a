0603264337bf226c98f65955bde97a2bf5d3a208
Chloe Seclier <seclier.chloe@gmail.com>
2025-06-03 16:59:39 +0200

    d

--- packages/frontend/web/src/pages/sign-up.tsx
+++ packages/frontend/web/src/pages/sign-up.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 import ButtonBlue from '@/components/button-blue';
 import Input from '@/components/input';
