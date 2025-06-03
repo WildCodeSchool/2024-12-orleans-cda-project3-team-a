c226c0986ccf2dbad67203865ed5b923695382a0
Chloe Seclier <seclier.chloe@gmail.com>
2025-06-03 11:14:34 +0200

    add link visitor in header

--- packages/frontend/web/src/components/nb-visitors-moons.tsx
+++ packages/frontend/web/src/components/nb-visitors-moons.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import moon from '../assets/images/icons-buttons/moon.png';
 import visitor from '../assets/images/icons-buttons/visitors.png';
 import { useGameInfoContext } from '../contexts/game-info-context';
@@ -10,7 +12,9 @@ export default function NbVisitorsMoons() {
     <div className='bg-secondary-gray flex h-8 w-fit cursor-default items-center justify-between gap-2 rounded px-2 py-0.5 text-xs shadow-[0px_4px_4px_rgba(0,0,0,0.25)] md:h-9 md:rounded-md md:text-base'>
       <div className='flex flex-row items-center gap-0.5 md:gap-1'>
         {countVisitorActiveFormated}
-        <img src={visitor} alt='visitors' className='h-6 md:h-7' />
+        <Link to='/visitors'>
+          <img src={visitor} alt='visitors' className='h-6 md:h-7' />
+        </Link>
       </div>
       <div
         className='flex flex-row items-center gap-0.5 md:gap-1'
