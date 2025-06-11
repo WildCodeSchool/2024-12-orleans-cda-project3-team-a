b7c10c577e2d42ae3e6b59e244b48cb783115cbd
SkymelTheo <erigoni.t@gmail.com>
2025-06-11 14:07:07 +0200

    test pour voir le texte en shrink

--- packages/frontend/web/src/components/welcome-guide.tsx
+++ packages/frontend/web/src/components/welcome-guide.tsx
@@ -4,6 +4,8 @@ import useCreaturesStatus from '@/hooks/use-creatures-status';
 
 export default function WelcomeGuide() {
   const { hasCreatures, checkCreaturesStatus } = useCreaturesStatus();
+  const isScreen = window.innerWidth > 950;
+
   useEffect(() => {
     void checkCreaturesStatus();
   }, [checkCreaturesStatus]);
@@ -12,12 +14,16 @@ export default function WelcomeGuide() {
     return null;
   }
   return (
-    <div className='absolute top-[65%] left-[50%] w-3/4 -translate-x-1/2 -translate-y-1/2 transform rounded-md bg-white/90 p-5 text-xs font-bold md:text-base'>
+    <div
+      className={`absolute top-[65%] left-[50%] ${isScreen ? 'w-3/4' : 'm:w-13/20'} -translate-x-1/2 -translate-y-1/2 transform rounded-md bg-white/90 p-5 text-xs font-bold md:text-base`}
+    >
       <div className='text-secondary-blue flex flex-col gap-3'>
-        <p className='whitespace-wrap md:animate-typing1 md:flex md:w-0 md:flex-wrap md:overflow-hidden md:whitespace-nowrap'>{`Now that you've bestowed a name upon your magical park, it's time to bring it to life with your very first creature!`}</p>
-        <p className='whitespace-wrap md:animate-typing2 md:flex md:w-0 md:flex-wrap md:overflow-hidden md:whitespace-nowrap'>{`For now, the Fairy realm is open to you.`}</p>
-        <p className='whitespace-wrap md:animate-typing3 md:flex md:w-0 md:flex-wrap md:overflow-hidden md:whitespace-nowrap'>{`To unlock the next realm, the "Winged" world, you'll need to buy at least one creature of each species into your park.`}</p>
-        <p className='whitespace-wrap md:animate-typing4 md:flex md:w-0 md:flex-wrap md:overflow-hidden md:whitespace-nowrap'>{`Let the adventure begin! ✨🐉`}</p>
+        <p className='whitespace-wrap md:animate-typing1 md:flex md:w-0 md:flex-wrap md:overflow-hidden md:whitespace-nowrap'>{`Now that you've bestowed a name upon your magical park.`}</p>
+        <p className='whitespace-wrap md:animate-typing2 md:flex md:w-0 md:flex-wrap md:overflow-hidden md:whitespace-nowrap'>{`It's time to bring  it to life with your very first creature!`}</p>
+        <p className='whitespace-wrap md:animate-typing3 md:flex md:w-0 md:flex-wrap md:overflow-hidden md:whitespace-nowrap'>{`For now, the Fairy realm is open to you.`}</p>
+        <p className='whitespace-wrap md:animate-typing4 md:flex md:w-0 md:flex-wrap md:overflow-hidden md:whitespace-nowrap'>{`To unlock the next realm, the "Winged" world.`}</p>
+        <p className='whitespace-wrap md:animate-typing5 md:flex md:w-0 md:flex-wrap md:overflow-hidden md:whitespace-nowrap'>{` You'll need to buyat least one creature of each species into your park.`}</p>
+        <p className='whitespace-wrap md:animate-typing6 md:flex md:w-0 md:flex-wrap md:overflow-hidden md:whitespace-nowrap'>{`Let the adventure begin! ✨🐉`}</p>
       </div>
       <div className='relative mt-4'>
         <div className='absolute -right-1 -bottom-3'>
