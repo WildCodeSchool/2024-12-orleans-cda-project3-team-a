4492f737e193d536c208c93f46507ed671b68fb5
YahyaBlm <yahya.belmou@gmail.com>
2025-06-11 14:48:58 +0200

    test path

--- packages/frontend/web/src/pages/world-enclosure.tsx
+++ packages/frontend/web/src/pages/world-enclosure.tsx
@@ -11,6 +11,9 @@ import Visitor from '@/components/visitor';
 import { useGameInfoContext } from '@/contexts/game-info-context';
 import useFetchBarriers from '@/hooks/use-fetch-barriers';
 
+import pathOfFourHor from '../assets/images/background/path-of-four-hor.png';
+import pathOfFourVert from '../assets/images/background/path-of-four-ver.png';
+
 export default function WorldEnclosure() {
   const { creaturesEnclos, decorations, unlockedZones } = useGameInfoContext();
   const { zone_id: zoneId } = useParams();
@@ -32,7 +35,17 @@ export default function WorldEnclosure() {
 
   return (
     <div className='relative flex min-w-[1200px] flex-wrap overflow-hidden md:w-full'>
-      <header className='fixed -right-2 z-3 flex gap-3 p-3 md:right-0'>
+      <img
+        src={pathOfFourHor}
+        alt=''
+        className='absolute z-2 min-w-[1200px] top-1/2 -translate-y-1/2'
+      />
+      <img
+        src={pathOfFourVert}
+        alt=''
+        className='absolute z-2 h-full left-1/2 -translate-x-1/2'
+      />
+      <header className='fixed -right-2 z-2 flex gap-3 p-3 md:right-0'>
         <InfoNbVisitorsMoons />
         <ReturnHome />
       </header>
@@ -50,11 +63,11 @@ export default function WorldEnclosure() {
           />
         );
       })}
-
+      
       {isLoading ? (
         <Loader />
       ) : (
-        <>
+        <div className='z-4'>
           {barriers.map((barrier) => {
             return (
               <Barrier
@@ -64,10 +77,10 @@ export default function WorldEnclosure() {
               />
             );
           })}
-        </>
+        </div>
       )}
 
-      <div className='absolute top-1/2 -translate-y-1/2'>
+      <div className='absolute top-1/2 -translate-y-1/2 z-3'>
         <Link to='/visitors'>
           <Visitor zoneId={zoneId} />
         </Link>
