39de4361779d9e6e9b234f4bf3d797f0c2997e90
Chloe Seclier <seclier.chloe@gmail.com>
2025-06-16 14:04:22 +0200

    change index to launch test one by one

--- packages/backend/api/src/index.ts
+++ packages/backend/api/src/index.ts
@@ -20,10 +20,13 @@ app.use(express.json());
 
 app.use('/api', router);
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server is listening on http://${HOST}:${PORT}`);
-});
+//allow to make test one by one
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(PORT, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Server is listening on http://${HOST}:${PORT}`);
+  });
+}
 
 declare module 'Express' {
   // eslint-disable-next-line @typescript-eslint/consistent-type-definitions
