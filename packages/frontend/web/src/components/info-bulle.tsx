81547c5378be4448e35a5e6afea0fad15e6d3b11
Chloe Seclier <seclier.chloe@gmail.com>
2025-06-17 14:57:39 +0200

    delete a div

--- packages/frontend/web/src/components/info-bulle.tsx
+++ packages/frontend/web/src/components/info-bulle.tsx
@@ -16,46 +16,45 @@ export default function InfoBulleHome() {
     .map((zone) => Number(zone.required_qty));
 
   return (
-    <div className='absolute top-1/2 right-5 flex -translate-y-1/2 items-center justify-center md:left-1/2 md:-translate-x-1/2'>
-      <div className='flex flex-col justify-center rounded-md bg-white px-0 py-2 text-xs shadow-[0px_4px_4px_rgba(0,0,0,0.25)] md:p-4 md:text-base'>
-        <div className='mb-4 flex flex-row items-center justify-center gap-4 font-extrabold'>
-          <img
-            className='h-7 w-7 md:h-10 md:w-10'
-            src='/images/minguch.png'
-            alt='Mingush'
-          />
-          <p className='text-secondary-blue hidden text-center md:flex'>
-            {'To unlock the next zone you need:'}
-          </p>
-        </div>
-        <div className='flex flex-col justify-center gap-4 md:flex-row'>
-          {/* Displays each creature species with its total number purchased */}
-          {creaturesMenu
-            .filter((creature) => creature.zone_id === countZoneIdUnlocked)
-            .map((creature) => (
-              <div
-                key={creature.id}
-                className='flex min-w-[60px] flex-col items-center'
+    <div className='absolute top-1/2 right-5 flex -translate-y-1/2 flex-col items-center justify-center rounded-md bg-white px-0 py-2 text-xs shadow-[0px_4px_4px_rgba(0,0,0,0.25)] md:left-1/2 md:-translate-x-1/2 md:p-4 md:text-base'>
+      <div className='mb-4 flex flex-row items-center justify-center gap-4 font-extrabold'>
+        <img
+          className='h-7 w-7 md:h-10 md:w-10'
+          src='/images/minguch.png'
+          alt='Mingush'
+          title='Mingush'
+        />
+        <p className='text-secondary-blue hidden text-center md:flex'>
+          {'To unlock the next zone you need:'}
+        </p>
+      </div>
+      <div className='flex flex-col justify-center gap-4 md:flex-row'>
+        {/* Displays each creature species with its total number purchased */}
+        {creaturesMenu
+          .filter((creature) => creature.zone_id === countZoneIdUnlocked)
+          .map((creature) => (
+            <div
+              key={creature.id}
+              className='flex min-w-[60px] flex-col items-center'
+            >
+              <img
+                src={`/images/creatures/${creature.src_image}`}
+                alt={creature.species}
+                className='h-5 w-5 md:h-10 md:w-10'
+              />
+              <p
+                className={`mt-1 text-xs md:text-base ${
+                  Number(creature.quantityCreature) >= Number(required)
+                    ? 'font-semibold text-green-500'
+                    : 'text-gray-600'
+                }`}
               >
-                <img
-                  src={`/images/creatures/${creature.src_image}`}
-                  alt={creature.species}
-                  className='h-5 w-5 md:h-10 md:w-10'
-                />
-                <p
-                  className={`mt-1 text-xs md:text-base ${
-                    Number(creature.quantityCreature) >= Number(required)
-                      ? 'font-semibold text-green-500'
-                      : 'text-gray-600'
-                  }`}
-                >
-                  {creature.quantityCreature}
-                  {'/'}
-                  {required}
-                </p>
-              </div>
-            ))}
-        </div>
+                {creature.quantityCreature}
+                {'/'}
+                {required}
+              </p>
+            </div>
+          ))}
       </div>
     </div>
   );
